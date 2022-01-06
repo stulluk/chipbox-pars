32d91f4dec6c5f9c72ddb669b4436589fa3965a6
Sertac TULLUK <stulluk@gmail.com>
2022-01-06 04:37:05 +0300

    build: move toolchain into container
    finally I decided to move toolchain
    inside the container so that
    I can use same container to build kernel and maybe uboot
    
    Signed-off-by: Sertac TULLUK <stulluk@gmail.com>

--- crosstool/gcc-3.4.6-glibc-2.3.6/arm-linux/arm-linux/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/opt/crosstool/gcc-3.4.6-glibc-2.3.6/arm-linux/arm-linux/lib"
-XML2_LIBS="-lxml2 -lz   -liconv -lm "
-XML2_INCLUDEDIR="-I/opt/crosstool/gcc-3.4.6-glibc-2.3.6/arm-linux/arm-linux/include/libxml2"
-MODULE_VERSION="xml2-2.9.0"
-
